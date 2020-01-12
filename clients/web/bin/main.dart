21ba68b3748ae81214afec80e48dceb52067e226
ericwindmill <ewindmill@greenbits.com>
2020-01-12 10:19:18 -0800

    adds site server oops

---
+++ clients/web/bin/main.dart
@@ -0,0 +1,20 @@
+import 'dart:io' show Platform;
+import 'dart:async' show runZoned;
+import 'package:path/path.dart' show join, dirname;
+import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_static/shelf_static.dart';
+
+void main() {
+  // Assumes the server lives in bin/ and that `webdev build` ran
+  var pathToBuild = join(dirname(Platform.script.toFilePath()), '..', 'build');
+
+  var handler = createStaticHandler(pathToBuild, defaultDocument: 'index.html');
+
+  var portEnv = Platform.environment['PORT'];
+  var port = portEnv == null ? 9999 : int.parse(portEnv);
+
+  runZoned(() {
+    io.serve(handler, '0.0.0.0', port);
+    print('Serving $pathToBuild on port $port');
+  }, onError: (e, stackTrace) => print('Oh noes! $e $stackTrace'));
+}
