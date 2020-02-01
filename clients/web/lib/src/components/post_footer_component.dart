0792be5c8c8f1b439f34ba729f4432a0be510c9a
ericwindmill <ewindmill@greenbits.com>
2020-02-01 11:47:07 -0800

    finish styling

--- clients/web/lib/src/components/post_footer_component.dart
+++ clients/web/lib/src/components/post_footer_component.dart
@@ -1,11 +1,15 @@
 import 'package:angular/angular.dart';
 import 'package:web/src/components/mail_chimp_form_component.dart';
+import 'package:web/src/components/social_links_component.dart';
 
 @Component(
   selector: 'post-footer',
   templateUrl: 'post_footer_component.html',
   styleUrls: ['post_footer_component.css'],
-  directives: [MailChimpFormComponent],
+  directives: [
+    MailChimpFormComponent,
+    SocialLinksComponent,
+  ],
   providers: [],
   pipes: [],
 )
